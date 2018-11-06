acf63d87f93852830c9ae90ba10ad7ce80589503
Andreas Zoglauer <andreas@megalibtoolkit.com>
2018-11-06 15:27:41 -0800

    CHG: File writting and batch mode

--- strippairing/explorelayouts.py
+++ strippairing/explorelayouts.py
@@ -39,13 +39,13 @@ parser = argparse.ArgumentParser(description='Perform training and/or testing of
 parser.add_argument('-f', '--file', default='StripPairing.x2.y2.strippairing.root', help='File name used for training/testing')
 #parser.add_argument('-c', '--complete', action='store_true', help='Try to find similar data files and train/test them too')
 parser.add_argument('-o', '--output', default='Results', help='Prefix for the output filename and directory')
-#parser.add_argument('-b', '--energy', default='0,10000', help='Energy bins. Example: 0,10000')
 parser.add_argument('-l', '--hiddenlayers', default='2', help='Number of hidden layers. Default: 2')
 parser.add_argument('-n', '--maximumnodes', default='50', help='Maximum number of nodes per hidden layer. Default: 50')
 #parser.add_argument('-a', '--algorithm', default='TMVA:BDT', help='Machine learning algorithm. Allowed: TMVA:MLP')
 parser.add_argument('-m', '--maxevents', default='100000', help='Maximum number of events to use')
 #parser.add_argument('-e', '--onlyevaluate', action='store_true', help='Only test the approach')
 #parser.add_argument('-t', '--onlytrain', action='store_true', help='Only train the approach')
+parser.add_argument('-b', '--batch', action='store_true', help='Batch mode - don\'t show any histograms')
 
 args = parser.parse_args()
 
@@ -76,29 +76,49 @@ for i in range(0, len(LayoutList)):
 
   
   if Passed == True:
-  #   # Store Performances in List
+    # Store Performances in List
     GoodSequences.append(PerformanceGoodSequences)
     BadSequences.append(PerformanceBadSequence)
+  else:
+    GoodSequences.append(0)
+    BadSequences.append(1)
 
 
-# Step 3: Make nice performance graphs
+# Step 3: Save the raw data to file:
+f = open(args.output + ".results",'w')
+f.write("# Hidden layers: {0}\n".format(args.hiddenlayers))
+f.write("# maximum nodes: {0}\n".format(args.maximumnodes))
+f.write("\n")
 
-# Simple histogram
-if int(args.hiddenlayers) == 1:
+counter = 0
+for X in list(itertools.product(range(15, int(args.maximumnodes)+1, 5), repeat=int(args.hiddenlayers))):
+  for e in X:
+    f.write(str(e) + " ")
+  f.write(" {0} {1}\n".format(GoodSequences[counter], BadSequences[counter]))
+  counter += 1
 
-  print([x/y for x, y in zip(GoodSequences, BadSequences)])
-  print(range(15, int(args.maximumnodes)+1, 5))
+f.close()
 
-  plt.plot(range(15, int(args.maximumnodes)+1, 5), [x/y for x, y in zip(GoodSequences, BadSequences)])
 
-  plt.title("Ratio Of Good to Bad Sequences", fontsize=20)
-  plt.xlabel('Nodes in Hidden Layer', fontsize=16)
-  plt.ylabel('Ratio of Good to Bad', fontsize=16)
+# Step 4: Make nice performance graphs
+
+if args.batch == False:
+  # Simple histogram
+  if int(args.hiddenlayers) == 1:
+
+    print([x/y for x, y in zip(GoodSequences, BadSequences)])
+    print(range(15, int(args.maximumnodes)+1, 5))
+    
+    plt.plot(range(15, int(args.maximumnodes)+1, 5), [x/y for x, y in zip(GoodSequences, BadSequences)])
+
+    plt.title("Ratio Of Good to Bad Sequences", fontsize=20)
+    plt.xlabel('Nodes in Hidden Layer', fontsize=16)
+    plt.ylabel('Ratio of Good to Bad', fontsize=16)
   
-  plt.show()
+    plt.show()
   
-else:
-  print("Plotting for more than one hidden layer not yet implemented")
+  else:
+    print("Plotting for more than one hidden layer not yet implemented")
   
 
 
