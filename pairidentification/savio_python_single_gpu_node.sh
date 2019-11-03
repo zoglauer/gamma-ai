8e11ad9de0d00b11c504c699007e7a9fee67b688
Harrison Costantino <harrisoncostantino@berkeley.edu>
2019-11-02 19:41:29 -0700

    changed script

--- pairidentification/savio_python_single_gpu_node.sh
+++ pairidentification/savio_python_single_gpu_node.sh
@@ -38,7 +38,7 @@ module load gcc/4.8.5 cmake python/3.6 tensorflow/1.12.0-py36-pip-gpu blas
 echo "Starting execution..."
 
 # --> ADAPT THE FILENAME
-python3 -u PairIdentification.py -f PairIdentification.p1.sim.gz -m 1500
+python3 -u PairIdentification.py -f PairIdentification.p1.sim.gz -m 1300
 
 
 echo "Waiting for all processes to end..."
