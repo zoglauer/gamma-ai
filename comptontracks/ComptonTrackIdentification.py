e311365f33d2cd15cea4f999a91bcdc03241d1af
Pranav Nagarajan <pranav.njn@berkeley.edu>
2019-11-05 19:08:28 -0800

    Added option of dropout layer

--- comptontracks/ComptonTrackIdentification.py
+++ comptontracks/ComptonTrackIdentification.py
@@ -249,34 +249,30 @@ input = tf.keras.layers.Input(batch_shape = (None, XBins, YBins, ZBins, 1))
 
 conv_1 = tf.keras.layers.Conv3D(64, 5, 2, 'valid')(input)
 batch_1 = tf.keras.layers.BatchNormalization()(conv_1)
-#max_1 = tf.keras.layers.maximum([batch_1, 0.1*batch_1])
 max_1 = tf.keras.layers.LeakyReLU(alpha = 0.1)(batch_1)
 
 conv_2 = tf.keras.layers.Conv3D(64, 3, 1, 'valid')(max_1)
 batch_2 = tf.keras.layers.BatchNormalization()(conv_2)
-#max_2 = tf.keras.layers.maximum([batch_2, 0.1*batch_2])
 max_2 = tf.keras.layers.LeakyReLU(alpha = 0.1)(batch_2)
 
 conv_3 = tf.keras.layers.Conv3D(128, 2, 2, 'valid')(max_2)
 batch_3 = tf.keras.layers.BatchNormalization()(conv_3)
-#max_3 = tf.keras.layers.maximum([batch_3, 0.1*batch_3])
 max_3 = tf.keras.layers.LeakyReLU(alpha = 0.1)(batch_3)
 
 conv_4 = tf.keras.layers.Conv3D(128, 2, 2, 'valid')(max_3)
 batch_4 = tf.keras.layers.BatchNormalization()(conv_4)
-#max_4 = tf.keras.layers.maximum([batch_4, 0.1*batch_4])
 max_4 = tf.keras.layers.LeakyReLU(alpha = 0.1)(batch_4)
 
-# pool_1 = tf.keras.layers.MaxPooling3D([2, 2, 2], strides = 2)(max_4)
-# conv_5 = tf.keras.layers.Conv3D(128, 2, 2, 'valid')(pool_1)
-
 #conv_reshape = tf.reshape(max_4, [-1, reduce(lambda a,b:a*b, max_4.shape.as_list()[1:])])
-#layer_num = reduce(lambda a,b:a*b, max_4.shape.as_list()[1:])
 
 reshape = tf.keras.layers.Flatten()(max_4)
 dense_1 = tf.keras.layers.Dense(64)(reshape)
 batch_5 = tf.keras.layers.BatchNormalization()(dense_1)
 activation = tf.keras.layers.ReLU()(batch_5)
+
+#drop = tf.keras.layers.Dropout(0.2)(activation)
+#dense_2 = tf.keras.layers.Dense(64)(drop)
+
 dense_2 = tf.keras.layers.Dense(64)(activation)
 
 print("      ... output layer ...")
