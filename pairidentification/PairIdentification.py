cf5313ebbb86dbddf5a59861c48e3a6e938a915c
Harrison Costantino <harrisoncostantino@berkeley.edu>
2019-11-17 18:14:55 -0800

    minor changes

--- pairidentification/PairIdentification.py
+++ pairidentification/PairIdentification.py
@@ -296,7 +296,7 @@ K.set_session(Session)
 # TODO: Add more robust model performance evaluation
 #TODO: Modularize tensor set up and try sequence for training
 
-BatchSize = 8
+BatchSize = 6
 
 
 print("Initializing Tensors...")
