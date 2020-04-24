d802ed1322ad12668ffed6464c4e2add6a8c7569
danielshin1 <danielshin@berkeley.edu>
2020-04-24 15:17:45 -0700

    Revert models/gnn.py

--- pairidentification/models/gnn.py
+++ pairidentification/models/gnn.py
@@ -72,17 +72,15 @@ class GNNSegmentClassifier(nn.Module):
         self.node_network = NodeNetwork(input_dim+hidden_dim, hidden_dim,
                                         hidden_activation)
 
-    def forward(self, X, Ri, Ro):
+    def forward(self, inputs):
         """Apply forward pass of the model"""
+        X, Ri, Ro = inputs
         # Apply input network to get hidden representation
-        print("apply input network")
         H = self.input_network(X)
         # Shortcut connect the inputs onto the hidden representation
-        print("inputs to hidden representation")
         H = torch.cat([H, X], dim=-1)
         # Loop over iterations of edge and node networks
         for i in range(self.n_iters):
-            print("iterate")
             # Apply edge network
             e = self.edge_network(H, Ri, Ro)
             # Apply node network
@@ -90,5 +88,4 @@ class GNNSegmentClassifier(nn.Module):
             # Shortcut connect the inputs onto the hidden representation
             H = torch.cat([H, X], dim=-1)
         # Apply final edge network
-        print("apply final network")
         return self.edge_network(H, Ri, Ro)
