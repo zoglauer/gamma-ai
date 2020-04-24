fd1a36108e21aa67e8542101a3bd2f802dad5c6a
danielshin1 <danielshin@berkeley.edu>
2020-04-24 13:14:23 -0700

    Add savio2_gpu script

---
+++ pairidentification/savio_gnn_test_savio2_gpu_2.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+#SBATCH -J Python
+
+#SBATCH --account=fc_cosi
+#SBATCH --partition=savio2_gpu
+#SBATCH --qos=savio_normal
+
+#SBATCH -t 01:00:00
+
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --gpus-per-task=2
+#SBATCH --gres=gpu:2
+
+#SBATCH --signal=2@60
+
+# --> CHANGE TO YOUR EMAIL
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=danielshin@berkeley.edu
+
+python3 -u PairIdentificationGNN.py --maxevents 2000 -b 200 --n_iters 100
+
