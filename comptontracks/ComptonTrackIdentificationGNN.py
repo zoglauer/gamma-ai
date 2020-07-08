9c39a6b25aab7441c4c10fdda259c86175749c4d
Pranav Nagarajan <pranav.njn@berkeley.edu>
2020-07-08 00:36:04 -0700

    Increasing batch size

--- comptontracks/ComptonTrackIdentificationGNN.py
+++ comptontracks/ComptonTrackIdentificationGNN.py
@@ -75,7 +75,7 @@ parser.add_argument('-f', '--filename', default='ComptonTrackIdentification_LowE
 parser.add_argument('-g', '--geometry', default='$(MEGALIB)/resource/examples/geomega/GRIPS/GRIPS.geo.setup', help='Geometry with which the sim file was created')
 parser.add_argument('-m', '--maxevents', default='10000', help='Maximum number of events to use')
 parser.add_argument('-s', '--testingtrainingsplit', default='0.1', help='Testing-training split')
-parser.add_argument('-b', '--batchsize', default='50', help='Batch size')
+parser.add_argument('-b', '--batchsize', default='64', help='Batch size')
 parser.add_argument('-e', '--epochs', default='100', help='Epochs')
 parser.add_argument('-t', '--tuning', default='0', help='Hyperparameter tuning mode')
 
@@ -455,8 +455,9 @@ model.evaluate([test_X, test_Ri, test_Ro], np.array(test_y), batch_size = BatchS
 
 eval_time = t.time() - start
 
-precision, recall, thresholds = precision_recall_curve(test_y[0], predictions[0])
-print(precision, recall, thresholds)
+for i in range(len(predictions)):
+    precisions, recalls, thresholds = precision_recall_curve(test_y[i], predictions[i])
+    print(precisions, recalls, thresholds)
 
 print("Time Elapsed for Data Loading: {}".format(dataload_time))
 print("Time Elapsed for Train/Test Split: {}".format(traintestsplit_time))
