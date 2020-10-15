7c2543af27f77b28c3313987861d82bf9e02f35a
Rithwik Sudharsan <rithwik@berkeley.edu>
2020-10-14 17:36:28 -0700

    bugfix

--- comptontracks/EvalGNN.py
+++ comptontracks/EvalGNN.py
@@ -2,14 +2,14 @@ import os
 import argparse
 
 parser = argparse.ArgumentParser(description='Evaluate GNN separately on gamma and electron tracks on log scale.')
-parser.add_argument('-l', '--low', default='5000', help='Starting num of events.')
-parser.add_argument('-h', '--high', default='10000000', help='Ending num of events.')
+parser.add_argument('-l', '--min', default='5000', help='Starting num of events.')
+parser.add_argument('-m', '--max', default='10000000', help='Ending num of events.')
 parser.add_argument('-f', '--file', default='ComptonTrackIdentification_LowEnergy.p1.sim.gz', help='Simulation filepath.')
 
 args = parser.parse_args()
 
-low = int(args.low)
-high = int(args.high)
+low = int(args.min)
+high = int(args.max)
 file = args.file
 
 print("=================== \nGNN Evaluation Script\n===================")
