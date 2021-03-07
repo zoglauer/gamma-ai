483fadce183b63fc9c13282db236443b776c5917
Rithwik Sudharsan <rithwik@berkeley.edu>
2021-03-06 17:57:39 -0800

    print total events

--- energylossestimate/EnergyLossEstimate.py
+++ energylossestimate/EnergyLossEstimate.py
@@ -190,7 +190,7 @@ class EnergyLossEstimate:
     PairEvents = []
 
     NEvents = 0
-    print("Total Events: {}".format(Reader.m_SimulatedEvents)
+    print("Total Events: {}".format(Reader.m_SimulatedEvents))
 
     while True: 
       print("   > {} Events Processed...".format(NEvents), end='\r')
