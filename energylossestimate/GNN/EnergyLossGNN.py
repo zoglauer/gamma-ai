92c394ed5260e16cb04cf4ad0a3d0970e7e65ad0
Rithwik Sudharsan <rithwik@berkeley.edu>
2021-03-08 03:37:42 -0800

    gnn adjustments

--- energylossestimate/GNN/EnergyLossGNN.py
+++ energylossestimate/GNN/EnergyLossGNN.py
@@ -99,7 +99,7 @@ if args.viz != "":
 else:
     viz_threshold = 0.5
 
-if int(args.maxevents) >= 100:
+if int(args.maxevents) >= 32:
   MaxEvents = int(args.maxevents)
 
 if int(args.batchsize) >= 16:
