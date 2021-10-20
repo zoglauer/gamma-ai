6a653c4949f1cca7975b50f307700dfd92dd7cee
JiYoo Jeong <jiyoojeong@berkeley.edu>
2021-10-20 03:04:47 -0700

    typo

--- energylossestimate/ShowerProfile.py
+++ energylossestimate/ShowerProfile.py
@@ -92,7 +92,7 @@ def shower_optimize(f, events, total_energies=None, initial_guesses=None):
 gamma_energies = [event.gamma_energy for event in event_list]
 initial_guesses = .5, .5 # TODO: set random seed an maybe pull from uniform dist. --> iterate over time to find best initial guess.
 # event_energies = [event.measured_energy for event in event_list]
-fitted_params, variance = shower_optimize(shower_profile, event_list, initial_guesses) #, gamma_energies)
+fitted_params, variance = shower_optimize(shower_profile, event_list, initial_guesses=initial_guesses) #, gamma_energies)
 alpha = fitted_params[0]
 beta = fitted_params[1]
 print('alpha:', alpha)
