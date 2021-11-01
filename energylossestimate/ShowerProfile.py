cc013d878ec7238f24cb0ec85283605c4e4719a7
JiYoo Jeong <jiyoojeong@berkeley.edu>
2021-11-01 12:15:40 -0700

    ja

--- energylossestimate/ShowerProfile.py
+++ energylossestimate/ShowerProfile.py
@@ -99,7 +99,7 @@ def build_xdat(events):
     start_pos = np.array([event.hits[0, 0:3] for event in event_list], dtype=np.float64) # use 0:3 instead of 2 for all x, y, z
     end_pos = np.array([event.hits[-1, 0:3] for event in event_list], dtype=np.float64) 
     #dist = np.abs(start_pos - end_pos)
-    dist = np.sqrt(np.linalg.norm(start_pos - end_pos, axis=1, ord=2)) # axis=2 if using multidim start and end pos
+    dist = np.sqrt(np.linalg.norm(start_pos - end_pos, axis=2, ord=2)) # axis=2 if using multidim start and end pos
     # dist = radiation length of the material.
     return (gamma_energies, dist) 
 
