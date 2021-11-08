fcd4d79eaec5ccefccc0b47c24fda529f57cabee
physicsnerd <heather.nerd@gmail.com>
2021-11-08 12:42:11 -0800

    some issues from prior commit remain, but most code written except for handling the geometry/bins

--- energylossestimate/shower_profile.py
+++ energylossestimate/shower_profile.py
@@ -39,26 +39,29 @@ si_x_min, si_x_max = -45.8, 45.8
 si_y_min, si_y_max = -48.3, 48.3
 si_z_min, si_z_max = 10.2, 45
 #OVERALL max/min?
+x_vals = [event.hits[0] for event in event_list]
+x_val_max, x_val_min = max(x_vals) + 5, min(x_vals) - 5
+y_vals = [event.hits[1] for event in event_list]
+y_val_max, y_val_min = max(y_vals) + 5, min(y_vals) - 5
+z_vals = [event.hits[2] for event in event_list]
+z_val_max, z_val_min = max(z_vals) + 5, min(z_vals) - 5
 
 #define x0 for tracker and calorimeter - units in cm
 tracker_x0 = 0.1 * 1.86 #90% is vacuum
 calorimeter_x0 = 9.37
 
-### func to find 't' for radiation depth - NOTE: NEEDS TO BE UNIT CHECKED
+### Find 't' for radiation depth - NOTE: NEEDS TO BE UNIT CHECKED
 
-#create bins as follows: - NOTE: MAY WISH TO MORE FINELY SLICE X/Y
-# z dimension: 0 to overall z dimension of detector, width of 0.5mm
-# x dimension: 0 to overall x dimension of detector, 'widths' being:
-# --> < silicon start, > silicon start and < silicon end, > silicon end
-# y dimension: 0 to overall y dimension of detector, 'widths' being:
-# --> < silicon start, > silicon start and < silicon end, > silicon end
-# each bin should have:
-# - (x, y, and z range)
-# - energy_inside init. to 0
-# - 't' init. to 0
-# data structure - hashmap, where the key includes material/location
-# function as value?
-#dictionary: stores different trained parameters
+# Create bins
+
+x_step, y_step, z_step = 1, 1, 1 #update? originally 0.5 mm width for z but...
+z_range = range(z_vals_min, z_vals_max, z_step)
+min_length = len(z_range)
+x_range = #
+z_range = #
+
+geometry = zip(x_range, y_range, z_range) #needs to end up with each tuple being x, y, z *range*
+
 # sort into xyz bins by numbering bins and adding that number to the
 # hit list and then just do dictionary[bin number] += hit_energy
 
@@ -66,80 +69,102 @@ calorimeter_x0 = 9.37
 # 1. find bin in which the hit goes based on (x,y,z)
 # 2. add energy for that hit to the energy_inside for the bin
 
+def bin_find(hit, geometry):
+    try:
+        return hit[5]
+    except IndexError:
+        #something
+
 def t_calculate(hits, geometry):
-    bins = {}
+    '''Finds t for each bin based off list of hits, then attaches t to appropriate hits.
+
+    Inputs: list of hits for an event, geometry.
+    Output: updated list of hits that now contains appropriate t for each hit.
+    Method:
+    - Generate bins, a dict where
+     - key: sequential numbers based off of geometry
+     - value: [measured_inside, t]
+    - Find t for each bin
+    - Go through original hit list and match hits to the appropriate bin and therefore appropriate t
+
+    NEEDS refactoring
+    '''
+    bins = {}#should be sequential numbers matched to [measured_inside, t]
     #sorts the hits into bins that hold energy in each bin based on geometry
     for column in bins:
         t = bins[column] / (area * zbin_height * x0) 
-        bins[i].append(t)
+        bins[column].append(t)
     # each t is now for each x y z bin
     # so we know from each hit, their xyz coords,
     # so we take the xzy bin t and apply it to each event hit
     for hit in hits:
+        bin_name = bin_find(hit, geometry)
+        t = bins[bin_name][1]
         hits[hit, 4] = t
-    return t#or hits??
+    return hits
+
+# Store t for each hit in EventData
+for event in event_list:
+    event.hits = t_calculate(event.hits, geometry)
 
 ### Fit for alpha, beta
 
-def shower_profile(event_inputs, alpha, beta):
+def shower_profile(event_hits, alpha, beta):
     '''Returns estimated gamma_energy based on inverse of shower profile eqn.
 
     Takes in:
-    - event_inputs, a tuple of data from EventData: (measured_energy, t)
+    - event_hits, a list of hits associated with that event
       - t is calculated from xyz coordinates of event hits and geometry
     - alpha, a fit parameter calculated across events
     - beta, a fit parameter calculated across events
     Returns:
     - predicted 'true' gamma energy
+
+    Method:
+    - sum across hits(measured[hit n] / [alpha/beta/[t for hit]]) = gamma [event]
     '''
-    measured_energy, t = event_inputs
     gamma = special.gamma(alpha)
-    t_beta_alpha = (beta * t)**(alpha - 1) * beta * np.exp(-1 * beta * t)
-    gamma_energy = (measured_energy * gamma) / t_beta_alpha
-    return gamma_energy
+    gamma_for_hits = []
+    for hit in event_hits:
+        hit[4] = t
+        hit[3] = measured_energy
+        t_beta_alpha = (beta * t)**(alpha - 1) * beta * np.exp(-1 * beta * t)
+        gamma_for_hits.append((measured_energy * gamma) / t_beta_alpha)
+    return sum(gamma_for_hits)
 
-#create list of measured/t/true based on events
-#measured_energies = [event.measured_energy for event in event_list]
-#gamma_energies = [event.gamma_energy for event in event_list]
+# Create list of measured/t/true based on events
 
-#gamma[event1] = measured[hit 1] * some weight + measured[hit2] * some weight ...
-# where some weight related to alpha beta and t somehow?
-# some weight = alpha-beta part of the shower profile function which includes t.
-# measured[for each hit] / [alpha/beta/[t for event]] = gamma energy[event 1]
-# sum across hits(measured[hit n] / [alpha/beta/[t for hit]]) = gamma energy[event]
+gamma_energies = [event.gamma_energy for event in event_list]
+event_hits = [event.hits for event in event_list]
 
-# store t for each hit in EventData, store shower_energy
+# TODO: set random seed an maybe pull from uniform dist. --> iterate over time to find best initial guess.
+initial_guesses = .5, .5, 1
+maxfev = 10000
 
-def shower_profile_fit(f, measured_energies, event_ts, gamma_energies):
+def shower_profile_fit(f, event_hits, gamma_energies, initial_guesses, maxfev):
     '''Find alpha and beta for shower_profile().
 
     Takes in:
     - f, should be shower_profile.
-    - 
+    - event_hits, which contains by proxy measured energy and t
+    - gamma_energies, a list of true energies
+    - initial_guesses, best guess of what parameters are
+    - maxfev, ?
     '''
-    # scipy fit
-    return alpha, beta
+    results, variance = optimize.curve_fit(f, (event_hits), gamma_energies, initial_guesses, maxfev=maxfev)
+    alpha = results[0]
+    beta = results[1]
+    return alpha, beta, variance
 
-#call shower_profile_fit appropriately and save alpha/beta, variance
-# store alpha/beta as class variable
-# ^ separately in EventData there should be a dictionary to match appropriate
-# alpha/beta with bin (? for later) [i.e. for the two different x0]
+# Call shower_profile_fit and save alpha/beta, variance
 
-### add predicted shower energy to event data instances
+alpha, beta, variance = shower_profile_fit(shower_profile, event_hits, gamma_energies, initial_guesses, maxfev)
+EventData.alpha = alpha
+EventData.beta = beta
+# dictionary for alpha/beta with bin [i.e. for the two different x0]?
+# call shower_profile_fit twice for different alpha/beta for different regions?
 
-def predicted_shower(event):
-    '''Calculates shower energy prediction for true energy.
-
-    Takes in:
-    - an event
-     - uses t, measured_energy, xyz of hits
-     - alpha, beta (class variables selected based on xyz of event)
-    '''
-    event.t = #calculate
-    event_inputs = (event.measured_energy, event.t)
-    alpha = event.alpha[]#select
-    beta = event.beta[]
-    return shower_profile(event.inputs, alpha, beta)
+### Add predicted shower energy to event data instances
 
 def error(event):
     '''Returns error in shower_energy prediction for given event.'''
@@ -148,7 +173,7 @@ def error(event):
 errors = []
 
 for event in event_list:
-    event.shower_energy = predicted_shower(event)
+    event.shower_energy = shower_profile(event.hits, alpha, beta)
     errors.append(error(event))
 
 avg_err = sum(errs)/len(event_list)
