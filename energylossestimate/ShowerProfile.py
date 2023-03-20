3ade279a03bb03ca809c884209b8362bccd0f8b3
Raiyan Rizwan <raiyanrizwan@berkeley.edu>
2023-03-19 21:29:02 -0700

    cleaning up code

--- energylossestimate/ShowerProfile.py
+++ energylossestimate/ShowerProfile.py
@@ -1,8 +1,7 @@
-from showerProfileUtils import parseTrainingData, get_num_files
-from showerProfileDataUtils import pickEvent, toDataSpace
-from DetectorGeometry import DetectorGeometry
+from showerProfileUtils import parseTrainingData
+from showerProfileDataUtils import pickEvent, toDataSpace, boundaryCheck, savePlot
 import matplotlib.pyplot as plt
-from sklearn.linear_model import RANSACRegressor, LinearRegression
+from sklearn.linear_model import RANSACRegressor
 from mpl_toolkits.mplot3d import Axes3D
 from scipy.spatial.distance import pdist
 import numpy as np
@@ -12,40 +11,33 @@ start_time = time.time()
 
 event_list = parseTrainingData()
 
-# BOUNDARY CHECK
-checks = []
-for event in event_list:
-    checks.extend([DetectorGeometry.verifyHit(hit) for hit in event.hits])
-
-print("Percentage in bounds: ", 100 * sum(checks) / len(checks))
-print("Number of hits out of bounds: ", len(checks) - sum(checks))
-
-# ANALYZING AN EVENT
+boundaryCheck(event_list)
 
 # event selection & data space
 event_to_analyze = pickEvent(False, event_list, lambda lst: len(lst)//2 + 10)
-D = toDataSpace(event_to_analyze)
+D, E = toDataSpace(event_to_analyze)
 
-# Matplotlib 3D scatter plot & RANSAC = outlier resistant regression model.
+# Matplotlib 3D scatter plot
 fig = plt.figure()
 ax = Axes3D(fig)
 
-# used to set residual threshold
-avg_distance = np.mean(pdist(D))
-
 # ransac model fit with test data
+avg_distance = np.mean(pdist(D))
 rs, mt = 2*avg_distance//5, len(D[:, 0])//2
 ransac = RANSACRegressor(residual_threshold=rs, max_trials=mt)
 xy = D[:, :2]
 z = D[:, 2]
 ransac.fit(xy, z)
 
-# inlier and outlier masks
+# inlier and outlier data
 inlier_mask = ransac.inlier_mask_
 outlier_mask = np.logical_not(inlier_mask)
+inlierD = D[inlier_mask, :]
+outlierD = D[outlier_mask, :]
+inlierE = np.array(E).T[inlier_mask]
 
-# scatter inlier data and regression plane
-ax.scatter(D[inlier_mask, 0], D[inlier_mask, 1], D[inlier_mask, 2], c='blue', label='Inliers')
+# scatter inlier data
+ax.scatter(inlierD[:, 0], inlierD[:, 1], inlierD[:, 2], c='blue', label='Inliers')
 
 # plot formattting
 ax.legend(loc='upper left')
@@ -54,8 +46,7 @@ ax.set_xlabel('Hit X (cm)')
 ax.set_ylabel('Hit Y (cm)')
 ax.set_zlabel('Hit Z (cm)')
 
-# LINEAR REGRESSION ATTEMPTS
-inlierD = np.column_stack((D[inlier_mask, 0], D[inlier_mask, 1], D[inlier_mask, 2]))
+# LINEAR REGRESSION
 # @source adapted StackOverFlow https://stackoverflow.com/questions/2298390/fitting-a-line-in-3d using chatGPT
 datamean = inlierD.mean(axis=0)
 uu, dd, vv = np.linalg.svd(inlierD - datamean)
@@ -76,20 +67,13 @@ zz = coef_x * xx + coef_y * yy + intercept
 ax.plot_surface(xx, yy, zz, alpha=0.5)
 """
 
-ax.scatter(D[outlier_mask, 0], D[outlier_mask, 1], D[outlier_mask, 2], c='red', label='Outliers')
+ax.scatter(outlierD[:, 0], outlierD[:, 1], outlierD[:, 2], c='red', label='Outliers')
 
 print('Plot finished!')
 print(f'Time: {round(time.time() - start_time, 2)} seconds')
 
-# identify file name
-directory = "showerProfilePlots"
-num_files = get_num_files(directory)
-
-# comment if using plt.show()
-plt.savefig(f"{directory}/consistent_hit_plot{num_files}.png")
-
-# uncomment to show plot @ higher res & interactable
-# plt.show()
+plt.show()
+# savePlot(plt, "showerProfilePlots", "consistent_hit_plot")
 
 # TODO: ransac reg fit line from inlier dataset
 # TODO: func (hit1, hit2) --> output distance, energy difference
--- energylossestimate/showerProfileDataUtils.py
+++ energylossestimate/showerProfileDataUtils.py
@@ -1,32 +1,51 @@
 import numpy as np
+from energylossestimate.DetectorGeometry import DetectorGeometry
+from energylossestimate.showerProfileUtils import get_num_files
+
+
+def savePlot(plt, directory, name):
+    num_files = get_num_files(directory)
+    plt.savefig(f"{directory}/{name}{num_files}.png")
 
 def pickEvent(random, event_list, selection):
+    """returns an event for analysis based on random or selection criteria"""
     event_to_analyze = event_list[selection(event_list)]
     if random:
         r = random.randint(0, len(event_list))
         event_to_analyze = event_list[r]
     return event_to_analyze
 
+def boundaryCheck(events):
+    """check if all hits are in the bounds of the detector"""
+    checks = []
+    for event in events:
+        checks.extend([DetectorGeometry.verifyHit(hit) for hit in event.hits])
+
+    print("Percentage in bounds: ", 100 * sum(checks) / len(checks))
+    print("Number of hits out of bounds: ", len(checks) - sum(checks))
+
 def toDataSpace(event):
+    """ Returns data in the form:
+           |         |
+           | | | | | |
+           | x y z E |
+           | | | | | |
+           |         |
+    where x, y, z, and E are horizontally stacked column vectors.
+    """
 
     x_vals = []
     y_vals = []
     z_vals = []
+    energies = []
 
     for hit in event.hits:
         x_vals.append(hit[0])
         y_vals.append(hit[1])
         z_vals.append(hit[2])
+        energies.append(hit[3])
 
     x_vals, y_vals, z_vals = map(np.array, [x_vals, y_vals, z_vals])
     D = np.column_stack((x_vals, y_vals, z_vals))
 
-    """
-       |       |
-       | | | | |
-       | x y z |
-       | | | | |
-       |       |
-    """
-
-    return D
+    return D, energies
