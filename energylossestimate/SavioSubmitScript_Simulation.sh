c1205df11602f5f35179ee6654ab9cfe85415940
Ethan Chen <ethanctrooper@gmail.com>
2024-03-26 01:12:09 -0300

    savio test 00:10:00 change -t --time

--- energylossestimate/SavioSubmitScript_Simulation.sh
+++ energylossestimate/SavioSubmitScript_Simulation.sh
@@ -7,12 +7,13 @@
 
 #SBATCH --account=fc_cosi
 #SBATCH --partition=savio3
-#SBATCH --qos=savio_debug
+#SBATCH --qos=savio_normal
 
 #SBATCH --chdir=/global/scratch/users/zoglauer/Sims/EnergyLossEstimate
 
 # This should give us 1,000,000 events 
-#SBATCH -t 10:00:00
+#SBATCH --time=00:10:00
+# Note changed from 10:00:00 to 00:10:00 for testing
 
 #SBATCH -N 1
 #SBATCH --ntasks-per-node=1
