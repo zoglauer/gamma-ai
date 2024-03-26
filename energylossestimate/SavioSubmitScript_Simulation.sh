6eddb4f3a379de8f82db13dc266de16084dbab2b
Ethan Chen <ethanctrooper@gmail.com>
2024-03-26 19:50:43 -0300

    simulation bash back to savio3

--- energylossestimate/SavioSubmitScript_Simulation.sh
+++ energylossestimate/SavioSubmitScript_Simulation.sh
@@ -6,7 +6,7 @@
 #SBATCH -J Sim
 
 #SBATCH --account=fc_cosi
-#SBATCH --partition=savio4_htc
+#SBATCH --partition=savio3
 #SBATCH --qos=savio_normal
 
 #SBATCH --chdir=/global/scratch/users/zoglauer/Sims/EnergyLossEstimate
