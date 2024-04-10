18aad99e34d2ef09f7549d4d393d598b9ae2a2ab
ethanctrooer <ethanctrooper@gmail.com>
2024-04-09 21:12:44 -0700

    added test debug file

---
+++ energylossestimate/SavioSubmitScript_Test.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Remember:
+# Submit via: sbatch ...
+
+#SBATCH -J Sim
+
+#SBATCH --account=fc_cosi
+#SBATCH --partition=savio3_htc
+#SBATCH --qos=savio_normal
+
+#SBATCH --chdir=/global/scratch/users/zoglauer/Sims/EnergyLossEstimate
+
+#SBATCH -t 00:10:00
+
+#SBATCH -N 1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+
+#SBATCH --signal=2@60
+
+#SBATCH --mail-user=ethan.chen@berkeley.edu
+#SBATCH --mail-type=ALL
+
+
+echo "Starting submit on host ${HOST}..."
+
+wait
